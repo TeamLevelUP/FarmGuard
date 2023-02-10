79e46d7af68f46baaf41deb9b6ca28a4bca69d96
DESKTOP-J13M1RV\82103 <rlaalstn1969@naver.com>
2023-02-10 11:56:31 +0900

    no message

--- Plant_Disease_Classifier/predict_image.py
+++ Plant_Disease_Classifier/predict_image.py
@@ -4,14 +4,19 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 
 # Load the saved model
-model = keras.models.load_model('model.h5')
+# model = keras.models.load_model('model.h5')
+model = keras.models.load_model('G:/내 드라이브/TUKorea/캡스톤디자인/model/model.h5')
 
 # Load an image file to classify
-img = image.load_img('input_image/common.jpg', target_size=(224, 224)) # 정상
-# img = image.load_img('input_image/disease.jpg', target_size=(224, 224)) # 질병
+try:
+    # img = image.load_img('input_image/common2.jpg', target_size=(224, 224)) # 정상
+    img = image.load_img('input_image/disease2.jpg', target_size=(224, 224)) # 질병
+except:
+    # img = image.load_img('input_image/common2.jpg', target_size=(224, 224)) # 정상
+    img = image.load_img('input_image/disease2.jpg', target_size=(224, 224))  # 질병
 img_array = np.array(img)
 img_array = img_array.astype('float32') / 255.0
-img_array = np.expand_dims(img_array, axis=0)
+img_array = np.expand_dims(img_array, axis = 0)
 
 # Use the model to make a prediction
 prediction = model.predict(img_array)
