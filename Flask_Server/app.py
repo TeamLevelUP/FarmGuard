762dcb76ec3436ff1f39f602848d99d5cff9d361
teastt <rlaalstn1969@naver.com>
2023-05-10 00:04:22 +0900

    230509/갤러리 및 갤러리이미지 판별기능 추가

--- Flask_Server/app.py
+++ Flask_Server/app.py
@@ -190,93 +190,123 @@ def checkIlum():
 
     return render_template('ilum.html', userid = session['userid'], **locals())
 
+@app.route('/gallery')
+def gallery():
+    return render_template("gallery.html", userid = session['userid'], **locals())
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['image']
-    if file:
-        # print(secure_filename(file.filename))
+    # gallery num is posted
+    # file의 이름을 파악함
+    if 'gallery' in request.form:
+        filename = request.form['gallery']
+        # print(filename)
+        # print(os.getcwd() + "/static/images/" + filename)
+        image_path = os.getcwd() + "/static/images/" + filename
+        image_path.replace('\\', '/')
 
+    # image is posted
+    # file을 저장하고 file의 이름을 파악함
+    elif 'image' in request.form:
+        file = request.files['image']
+        # 사진이 업로드되있지 않으면
+        if not file:
+            return '''
+                            <script>
+                                // 경고창 
+                                alert("이미지 업로드 실패")
+                                // 이전페이지로 이동
+                                history.back()
+                            </script>
+                        '''
+        # print(secure_filename(file.filename))
         # Save the img file
         filename = secure_filename(file.filename)
+        # print(type(filename)) # str
+        # print(filename)       # xx.jpg
         file.save(os.path.join("static/images", "input_image.jpg"))
         # file.save(os.path.join("static/images", filename))
-        os.system("cp static/images/input_image.jpg yolov5/data/images/input_image.jpg")
+    # wrong post
+    # return
+    else:
+        return '''
+                            <script>
+                                // 경고창 
+                                alert("ERROR")
+                                // 이전페이지로 이동
+                                history.back()
+                            </script>
+                        '''
 
-        # 기존에 사용한 폴더 지우기
-        # print(os.getcwd()) # C:\FarmGuard\Flask_Server
-        # os.system("cd yolov5/runs/detect")
-        if os.path.isdir('yolov5/runs/detect/exp'):
-            for file in os.listdir("yolov5/runs/detect/exp"):
-                os.remove('yolov5/runs/detect/exp/' + file)
-            os.rmdir('yolov5/runs/detect/exp')
+    # yolo학습을 위해 이미지파일 복사
+    os.system("cp static/images/" + filename + " yolov5/data/images/input_image.jpg")
 
-        # 감지 여부 확인 - 감지 했으면 True 못했으면 False
-        opt = detect.parse_opt()
-        # detect.main: image detection and save file, return 검출 여부
-        is_detected = detect.main(opt)
-        # print(is_detected)
+    # 기존에 사용한 폴더 지우기
+    # print(os.getcwd()) # C:\FarmGuard\Flask_Server
+    # os.system("cd yolov5/runs/detect")
+    if os.path.isdir('yolov5/runs/detect/exp'):
+        for file in os.listdir("yolov5/runs/detect/exp"):
+            os.remove('yolov5/runs/detect/exp/' + file)
+        os.rmdir('yolov5/runs/detect/exp')
+
+    # 감지 여부 확인 - 감지 했으면 True 못했으면 False
+    opt = detect.parse_opt()
+    # detect.main: image detection and save file, return 검출 여부
+    is_detected = detect.main(opt)
+    # print(is_detected)
         
-        # 이미지에서 상추 검출 안됐을시 리턴
-        if not is_detected:
-            return '''
-                        <script>
-                            // 경고창 
-                            alert("상추 검출 실패, 다른 이미지로 다시 시도하세요")
+    # 이미지에서 상추 검출 안됐을시 리턴
+    if not is_detected:
+        return '''
+                    <script>
+                        // 경고창 
+                        alert("상추 검출 실패, 다른 이미지로 다시 시도하세요")
                             // 이전페이지로 이동
-                            history.back()
-                        </script>
-            '''
+                        history.back()
+                    </script>
+        '''
 
-        # 이미지에서 상추 검출시 질병 예측 시작
+    # keras-이미지에서 상추 검출시 질병 예측 시작
 
-        # Load the saved model
-        # model = keras.models.load_model('model/model_class2_epoch7_dropout0.2.h5')
-        model = keras.models.load_model('model/model_class3_epoch10_dropout0.2.h5')
+    # Load the saved model
+    # model = keras.models.load_model('model/model_class2_epoch7_dropout0.2.h5')
+    model = keras.models.load_model('model/model_class3_epoch10_dropout0.2.h5')
 
-        # Load an image file to classify
-        # img = image.load_img('input_image/common2.jpg', target_size=(224, 224)) # 정상
-        # img = image.load_img('input_image/disease2.jpeg', target_size=(224, 224)) # 질병
+    # Load an image file to classify
+    # img = image.load_img('input_image/common2.jpg', target_size=(224, 224)) # 정상
+    # img = image.load_img('input_image/disease2.jpeg', target_size=(224, 224)) # 질병
 
-        # print(link) # .jpg까지 저장됨
-        # img = image.load_img(f'static/images/{filename}', target_size=(224, 224))
-        img = image.load_img('static/images/input_image.jpg', target_size=(224, 224))
+    # print(link) # .jpg까지 저장됨
+    # img = image.load_img(f'static/images/{filename}', target_size=(224, 224))
+    img = image.load_img('static/images/input_image.jpg', target_size=(224, 224))
 
-        img_array = np.array(img)
-        img_array = img_array.astype('float32') / 255.0
-        img_array = np.expand_dims(img_array, axis = 0)
+    img_array = np.array(img)
+    img_array = img_array.astype('float32') / 255.0
+    img_array = np.expand_dims(img_array, axis = 0)
 
-        # Use the model to make a prediction
-        prediction = model.predict(img_array)
-        predicted_class = np.argmax(prediction[0])
+    # Use the model to make a prediction
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction[0])
 
-        # predict result
-        # disease_list = ['normal', 'disease']
-        # sclerotiniarot(균핵병) downymildew(노균병)
-        disease_list = {0:'normal', 1:'sclerotiniarot', 2:'downymildew'}
-        # print("Prediction: ", prediction)
-        # print("Predicted class:", predicted_class)
-        disease_class = disease_list[int(predicted_class)]
-        # return redirect(url_for('upload_success', filename = filename, disease_class = disease_class))
-        # print(len(session)) # 0
+    # predict result
+    # sclerotiniarot(균핵병) downymildew(노균병)
+    disease_list = {0:'normal', 1:'sclerotiniarot', 2:'downymildew'}
+    # print("Prediction: ", prediction)
+    # print("Predicted class:", predicted_class)
+    disease_class = disease_list[int(predicted_class)]
+    # return redirect(url_for('upload_success', filename = filename, disease_class = disease_class))
+    # print(len(session)) # 0
         
-        # 화면에 Bounding Box를 포함한 이미지 출력을 위해 복사
-        os.system("cp yolov5/runs/detect/exp/input_image.jpg static/images/input_image.jpg")
+    # 화면에 Bounding Box를 포함한 이미지 출력을 위해 복사
+    os.system("cp yolov5/runs/detect/exp/input_image.jpg static/images/input_image.jpg")
 
-        if 'userid' in session:
-            return render_template('success.html',
+    if 'userid' in session:
+        return render_template('success.html',
                                filename = filename, disease_class = disease_class, userid = session['userid'])
-        else:
-            return render_template('success.html',
-                                   filename=filename, disease_class=disease_class)
+    else:
+        return render_template('success.html',
+                               filename=filename, disease_class=disease_class)
 
-    return '''
-                <script>
-                    // 경고창 
-                    alert("이미지 업로드 실패")
-                    // 이전페이지로 이동
-                    history.back()
-                </script>
-            '''
 
 # @app.route('/success/<filename>')
 # def upload_success(filename, disease_class):
